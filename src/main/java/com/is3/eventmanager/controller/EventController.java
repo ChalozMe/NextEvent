3645ea52aa7b3c7c65298f5e97e1d6097a372b65
ChalozMe <ccarpiop@unsa.edu.pe>
2026-06-27 17:18:58 -0500

    feat: create EventController, changed security.java

---
+++ src/main/java/com/is3/eventmanager/controller/EventController.java
@@ -0,0 +1,25 @@
+package com.is3.eventmanager.controller;
+
+import com.is3.eventmanager.dto.EventRequest;
+import com.is3.eventmanager.service.EventService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @PostMapping
+    public String createEvent(@RequestBody EventRequest request) {
+
+        eventService.create(request);
+
+        return "Event created";
+    }
+
+}
